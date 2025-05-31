1dc85844dde0ca0a336a440ebd15e811df10fa06
udit khimesra <udit.khimesra@gmail.com>
2025-05-31 12:10:29 +0530

    UrlShortening Service

---
+++ URLShorteningService/URLShorteningService/Controllers/URLController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace URLShorteningService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class URLController : ControllerBase
+    {
+        private readonly ILogger<URLController> _logger;
+
+        public URLController(ILogger<URLController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string GetActualURL(string shortUrl)
+        {
+            return String.Empty;
+        }
+
+
+        [HttpPut]
+        public string MapPath(string url)
+        {
+            return String.Empty;
+        }
+    }
+}
